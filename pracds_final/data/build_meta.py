2f4eed738c7c5b84580136af99092fe955b1dea3
Adithya Balaji <adithyabsk@gmail.com>
2021-05-06 01:50:39 -0400

    Add gitkeeps and DVC metadata generation script
    * Add gitkeeps for external, interim, processed, and raw data
    * Add dvc.lock which specifies when the data in the remote was last
    updated
    * Add dvc.yaml which specifies how data files are generated and
    transformed
    * Rename xeno_canto_api.py to build_meta.py

--- pracds_final/data/build_meta.py
+++ pracds_final/data/build_meta.py
@@ -8,6 +8,7 @@ import click
 import pandas as pd
 import requests
 import yaml
+from click import BadParameter
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from tqdm import tqdm
@@ -17,9 +18,7 @@ JSON = Dict[str, Any]
 
 BASE_URL = URL("https://www.xeno-canto.org/api/2")
 FILE_PATH = Path(__file__).parent
-RAW_DATA_PATH = FILE_PATH / "../../data/raw"
 PARAMS_PATH = FILE_PATH / "../../params.yaml"
-META_DATA_FILENAME = "metadata.csv"
 
 
 def get_page_recording(song_url: URL) -> JSON:
@@ -66,9 +65,7 @@ def search_recordings(**query_params) -> List[JSON]:
         num_pages = resp_json["numPages"]
         recordings = resp_json["recordings"]
         if num_pages > 1:
-            page_urls = [
-                url.update_query(page=p) for p in range(2, 4)
-            ]  # num_pages + 1)]
+            page_urls = [url.update_query(page=p) for p in range(2, num_pages + 1)]
             with ProcessPoolExecutor(max_workers=10) as ppe:
                 recordings.extend(
                     itertools.chain(
@@ -98,23 +95,27 @@ def save_recordings_metadata(song_list: List[JSON], output_path: Path):
 
 
 @click.command()
-@click.argument("output_filepath", type=click.Path(exists=True))
-def main(output_filepath):
+@click.argument("output_file", type=click.Path(dir_okay=False))
+def main(output_file):
     """Generate the audio meta data.
 
     Uses the parameters from `params.yaml` to build a csv of recordings
 
     """
+    output_file = Path(output_file)
+    if not output_file.parent.exists():
+        raise BadParameter("output_filepath directory must exist.")
+
     # Load DVC Params file
     with open(PARAMS_PATH) as params_file:
-        params_dict = yaml.load(params_file)
+        params_dict = yaml.safe_load(params_file)
 
     # Extract the Xeno Canto list of queries to filter initial data and build a kwarg dict
     query_list = params_dict["build"]["meta"]["queries"]
     qp = {"query": "+".join(query_list)}
     # Search for the recordings using the queries and save the data
     rs = search_recordings(**qp)
-    save_recordings_metadata(rs, output_filepath)
+    save_recordings_metadata(rs, output_file)
 
 
 if __name__ == "__main__":
